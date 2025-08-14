5f795128e1b57372c0cc0f662b28de8c8964046e
abneeeees <avneeshsingh288@gmail.com>
2025-08-14 23:05:41 +0530

    added dataset of several cards as well

--- Deck_of_Wars/game/management/commands/commands.py
+++ Deck_of_Wars/game/management/commands/commands.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand
-from game.models import Deck 
+from game.models import Deck , Card
 import json
 import os
 from django.conf import settings
@@ -15,15 +15,38 @@ class Command(BaseCommand):
         )
 
     def handle(self , *agrs , **kwargs):
-        path = os.path.join( settings.BASE_DIR ,  "game" , "data", "deck.json")
-        with open(path) as f:
-            deck_data = json.load(f)
+        deck_path = os.path.join( settings.BASE_DIR ,  "game" , "data", "deck.json")
+        ben_path = os.path.join( settings.BASE_DIR ,  "game" , "data", "ben_10.json")
 
         if kwargs['del']:
             Deck.objects.all().delete()
+            Card.objects.all().delete()
 
+        # for deck
+        with open(deck_path) as f:
+            deck_data = json.load(f)
 
         for data in deck_data:
             name = data["name"]
             Deck.objects.update_or_create(name=name)
+
+        #for ben10
+        with open(ben_path) as f:
+            ben_data = json.load(f)
+
+        ben_deck , _ = Deck.objects.update_or_create(name="Ben 10")
+
+        for data in ben_data:
+            Card.objects.update_or_create(
+                deck=ben_deck,
+                Rank=data["Rank"],
+                defaults={
+                    "Name": data["Name"],
+                    "Power": data["Power"],
+                    "Strength": data["Strength"],
+                    "Speed": data["Speed"],
+                    "Intelligence": data["Intelligence"],
+                    "Height": data["Height"]
+                }
+            )
 
--- Deck_of_Wars/game/models.py
+++ Deck_of_Wars/game/models.py
@@ -8,17 +8,17 @@ class Deck(models.Model):
 
 class Card(models.Model):
     deck = models.ForeignKey(Deck, on_delete=models.CASCADE)
-    name = models.CharField(max_length=200)
-    rank = models.IntegerField()
-    power = models.IntegerField()
-    strength = models.IntegerField()
-    speed = models.IntegerField()
-    intelligence = models.IntegerField()
-    height = models.CharField(max_length=50)
+    Name = models.CharField(max_length=200)
+    Rank = models.IntegerField()
+    Power = models.IntegerField()
+    Strength = models.IntegerField()
+    Speed = models.IntegerField()
+    Intelligence = models.IntegerField()
+    Height = models.CharField(max_length=50)
 
     def __str__(self):
-        return f"{self.name} - {self.rank}"
+        return f"{self.Name} - {self.Rank}"
 
     class Meta:
-        ordering = ['deck' , 'rank']
-        unique_together = ['deck' , 'rank']
+        ordering = ['deck' , 'Rank']
+        unique_together = ['deck' , 'Rank']
